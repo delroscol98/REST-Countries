fe65f29d394b0a2368fec67dfb65bf78510c3442
delroscol98 <collindelrosario98@gmail.com>
2024-08-28 23:31:54 +1000

    CountriesHeader dropdown

--- src/components/CountriesHeader/CountriesHeader.jsx
+++ src/components/CountriesHeader/CountriesHeader.jsx
@@ -1,16 +1,46 @@
 import styles from "./CountriesHeader.module.css";
-import Icon from "../../../public/images/search-outline.svg";
+import SearchIcon from "../../../public/images/search-outline.svg";
+import ChevronIcon from "../../../public/images/chevron-down-outline.svg";
+import { useState } from "react";
 
 function CountriesHeader() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenDropdown = () => {
+    setIsOpen(() => !isOpen);
+  };
+
   return (
     <div className={styles.countriesHeader}>
       <div className={styles.countriesHeader__searchContainer}>
-        <Icon className={styles.countriesHeader__searchIcon} />
+        <SearchIcon className={styles.countriesHeader__searchIcon} />
         <input
           className={styles.countriesHeader__searchInput}
           type="text"
           placeholder="Search for a country..."
         />
+      </div>
+      <div className={styles.countriesHeader__dropdown}>
+        <button
+          className={styles.countriesHeader__dropdownBtn}
+          onClick={handleOpenDropdown}
+        >
+          Filter by Region
+          <ChevronIcon className={styles.countriesHeader__dropdownIcon} />
+        </button>
+        {isOpen && (
+          <ul className={styles.countriesHeader__dropdownList}>
+            <li className={styles.countriesHeader__dropdownListItem}>Africa</li>
+            <li className={styles.countriesHeader__dropdownListItem}>
+              America
+            </li>
+            <li className={styles.countriesHeader__dropdownListItem}>Asia</li>
+            <li className={styles.countriesHeader__dropdownListItem}>Europe</li>
+            <li className={styles.countriesHeader__dropdownListItem}>
+              Oceania
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
