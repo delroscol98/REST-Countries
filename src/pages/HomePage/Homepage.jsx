da8cef354895c4981a0e980a80864b630e7750a3
delroscol98 <collindelrosario98@gmail.com>
2024-09-07 23:51:35 +1000

    conditional rendering of pagination

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -3,15 +3,18 @@ import Countries from "../../components/Countries/Countries";
 import CountriesHeader from "../../components/CountriesHeader/CountriesHeader";
 import CountriesList from "../../components/CountriesList/CountriesList";
 import Pagination from "../../components/Pagination/Pagination";
+import useCountries from "../../hooks/useCounties";
 
 function Homepage() {
+  const { countriesList, countriesPerPage } = useCountries();
+  const pageNums = Math.ceil(countriesList.length / countriesPerPage);
   return (
     <main>
       <Header />
       <Countries>
         <CountriesHeader />
         <CountriesList />
-        <Pagination />
+        {pageNums > 1 && <Pagination />}
       </Countries>
     </main>
   );
